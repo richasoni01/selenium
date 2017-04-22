53876ec3c15866f052411fd39c302baa0dbeff4b
RichaSoni <sonirich01@gmail.com>
2017-04-22 18:36:00 +0530

    rajmachi

--- src/practice/united.java
+++ src/practice/united.java
@@ -8,7 +8,7 @@ Dog2 d=new Dog2();
 d.strSplit();
 	}
 public String strSplit(){
-	String dmo= "we are united gandi airlines fully";
+	String dmo= "we rajamchi fully";
 	
 	String h=dmo.replaceAll("-","\n");
 	System.out.println(h);
