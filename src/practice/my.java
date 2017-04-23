ab6bcc8eff7fdd75cb25edc3638ae250c4c59abd
RichaSoni <sonirich01@gmail.com>
2017-04-23 16:16:11 +0530

    diagram

--- src/practice/my.java
+++ src/practice/my.java
@@ -8,7 +8,7 @@ Dog2 d=new Dog2();
 d.strSplit();
 	}
 public String strSplit(){
-	String dmo= "we are gaurav soni";
+	String dmo= "we are gaurav kt from git soni";
 	
 	String h=dmo.replaceAll("-","\n");
 	System.out.println(h);
