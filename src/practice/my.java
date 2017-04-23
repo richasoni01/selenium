e656e60786b55089c4f7e1a5111215b06f9a8319
RichaSoni <sonirich01@gmail.com>
2017-04-23 17:37:11 +0530

    gadha

--- src/practice/my.java
+++ src/practice/my.java
@@ -8,7 +8,7 @@ Dog2 d=new Dog2();
 d.strSplit();
 	}
 public String strSplit(){
-	String dmo= "we are gaurav posotive kt from git soni/vgvhjghg";
+	String dmo= "we are not bevkoof";
 	
 	String h=dmo.replaceAll("-","\n");
 	System.out.println(h);
