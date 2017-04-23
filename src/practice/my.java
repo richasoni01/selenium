cc7f4b9ec9c23543281251d7c089954bd1a9c06c
RichaSoni <sonirich01@gmail.com>
2017-04-23 16:29:21 +0530

    goanywhere

--- src/practice/my.java
+++ src/practice/my.java
@@ -8,7 +8,7 @@ Dog2 d=new Dog2();
 d.strSplit();
 	}
 public String strSplit(){
-	String dmo= "we are gaurav kt from git soni";
+	String dmo= "we are gaurav posotive kt from git soni/vgvhjghg";
 	
 	String h=dmo.replaceAll("-","\n");
 	System.out.println(h);
